c3199c05e86717ba68151e67cc4ff832e2601819
Dex Bleeker <dex@bleeker.nl>
2021-10-29 10:48:39 +0200

    mpeck function

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import random
+import mmh3
 
 
 class Client:
@@ -29,6 +30,19 @@ class Client:
     def client_id(self):
         """The id of the client"""
         return self.client_id
+
+    def mpeck(self, keyword_set):
+        h = [mmh3.hash(x, 0) for x in keyword_set]
+        f = [mmh3.hash(x, 1) for x in keyword_set]
+
+        s = random.randrange(start=1, stop=self.prime - 1)
+        r = random.randrange(start=1, stop=self.prime - 1)
+
+        a = pow(self.generator, r, self.prime)
+        bs = [pow(key, s) for key in [self.y_a, self.server.user_public_key(0)]]
+        cs = [pow(h[i], r) * pow(f[i], s) for i in range(len(h))]
+
+        return [a, *bs, *cs]
 
     def encrypt(self, message, user_id=0):
         NotImplementedError("Class %s doesn't implement encrypt()" % self.__class__.__name__)
