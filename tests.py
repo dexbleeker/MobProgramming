a5ad87ea57ff28231c5e701c8c838ae63ea487d5
Dex Bleeker <dex@bleeker.nl>
2021-11-15 14:43:02 +0100

    New test

--- tests.py
+++ tests.py
@@ -72,6 +72,12 @@ class Test(unittest.TestCase):
 
         self.assertEqual(self.server.data[user.user_id()], [(sigma, m_peck)])
 
+    def test_data_storing_consultant(self):
+        """
+        Test whether the consultant can properly store data
+        """
+        pass
+
     def test_data_querying(self):
         """
         Test whether querying works.
@@ -114,6 +120,42 @@ class Test(unittest.TestCase):
         result = self.server.evaluate_trapdoor(trapdoor, user2.user_id())
         self.assertEqual(len(result), 0)
         self.assertEqual(result, [])
+
+    def test_data_query_for_consultant(self):
+        """
+        The consultant should get all the results.
+        """
+        user1 = random.choice(self.users)
+        user2 = random.choice(self.users)
+        while user1 == user2:
+            user2 = random.choice(self.users)
+
+        sigma1 = user1.encrypt(123456)
+        m_peck1 = user1.m_peck(["foobar"])
+        self.server.store_data(user1.user_id(), (sigma1, m_peck1))
+
+        sigma2 = user2.encrypt(654321)
+        m_peck2 = user2.m_peck(["foobar"])
+        self.server.store_data(user2.user_id(), (sigma2, m_peck2))
+
+        # User 1 should only get his result
+        trapdoor1 = user1.generate_trapdoor([0], ["foobar"])
+        result1 = self.server.evaluate_trapdoor(trapdoor1, user1.user_id())
+        self.assertEqual(len(result1), 1)
+        self.assertEqual(result1[0], sigma1)
+        # Same for user 2
+        trapdoor2 = user2.generate_trapdoor([0], ["foobar"])
+        result2 = self.server.evaluate_trapdoor(trapdoor2, user2.user_id())
+        self.assertEqual(len(result2), 1)
+        self.assertEqual(result2[0], sigma2)
+
+        # Now for the interesting part:
+        # The consultant should het both results.
+        trapdoor3 = self.consultant.generate_trapdoor([0], ["foobar"])
+        result3 = self.server.evaluate_trapdoor(trapdoor3, 0)
+        self.assertEqual(len(result3), 2)
+        self.assertEqual(result3[0], sigma1)
+        self.assertEqual(result3[1], sigma2)
 
     def test_text_file(self):
         """
